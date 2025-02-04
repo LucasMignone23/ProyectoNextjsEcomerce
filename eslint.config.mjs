b719f68b13b50b9df7bcfcc83285105463281f2c
Lucas Mignone <lucasmignone.lm@gmail.com>
2025-02-03 21:45:52 -0300

    modificacion

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-const { dirname } = require("path");
-const { fileURLToPath } = require("url");
-const { FlatCompat } = require("@eslint/eslintrc");
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,4 +11,4 @@ const compat = new FlatCompat({
 
 const eslintConfig = [...compat.extends("next/core-web-vitals")];
 
-module.exports = eslintConfig;
+export default eslintConfig;
